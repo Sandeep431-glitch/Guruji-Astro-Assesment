89049c6cb8962f6e6e2019d63832781aee54b889
Sandeep Ghosh <68499426+Sandeep431-glitch@users.noreply.github.com>
2023-06-03 21:08:37 +0530

    First time uploaded

---
+++ guruji-astro-homepage/src/App.js
@@ -0,0 +1,17 @@
+import Astrologer from './components/gurujiAstroCards';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import './App.module.css';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Astrologer />
+      <Footer />
+    </>
+
+  );
+}
+
+export default App;
